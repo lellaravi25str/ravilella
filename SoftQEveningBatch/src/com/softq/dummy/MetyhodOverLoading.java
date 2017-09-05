43ad35381fe93b39b299d55fc3dcfb37ede4c4e6
ravilella <lellaravi25ster@gmail.com>
2017-09-05 11:51:33 +0530

    removed some functions

--- SoftQEveningBatch/src/com/softq/dummy/MetyhodOverLoading.java
+++ SoftQEveningBatch/src/com/softq/dummy/MetyhodOverLoading.java
@@ -3,29 +3,11 @@ package com.softq.dummy;
 public class MetyhodOverLoading 
 {
 
-	/*public void m1(int i)
-	{
-		System.out.println("int arg method:" +i);
-	}*/
-	
-	
 	int m1(float f)
 	{
 		System.out.println("float arg method");
 		return 10;
 	}
-	
-	
-	/*public void m1(String s)
-	{
-		System.out.println("string arg method");
-	}*/
-	
-	
-	/*public void m1(Object o)
-	{
-		System.out.println("string arg method");
-	}*/
 
 	
 	public static void main(String[] args) 
@@ -36,7 +18,7 @@ public class MetyhodOverLoading
 		a.m1(10.90f);
 		
 		String s=new String("ravi");
-		a.m1(s);
+		
 
 	}
 
