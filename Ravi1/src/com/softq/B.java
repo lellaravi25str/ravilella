b5cd2b3ac3cd99cefff9575e7f2996a16aaeea79
ravilella <lellaravi25ster@gmail.com>
2017-09-06 06:34:31 +0530

    aa

--- Ravi1/src/com/softq/B.java
+++ Ravi1/src/com/softq/B.java
@@ -1,28 +1,44 @@
 package com.softq;
 
 public class B extends A
+
 {
 
-	public void m2()
-	{
-		System.out.println("Hi iam m2 method from class B");
-		System.out.println("Hi iam updated method ");
-		
-		System.out.println("Hi iam updated method ");
-		System.out.println("Hi iam updated method ");
-		System.out.println("Hi iam updated method ");
-	}
 	
-	public static void main(String[] args) 
-	{
-		A a=new A();
-		a.m1();
+public void m2()
+	
+{
 		
-		B b=new B();
-		b.m1();
-		b.m2();
+System.out.println("Hi iam m2 method from class B");
+		
+System.out.println("Hi iam updated method ");
+		
+		System.out.println("Hi iam updated method ");
+		
+System.out.println("Hi iam updated method ");
+		
+System.out.println("Hi iam updated method ");
+	
+}
+	
+	
+public static void main(String[] args) 
+	
+{
+		
+A a=new A();
+		
+a.m1();
+		
+		
+B b=new B();
+		
+b.m1();
+		
+b.m2();
 		
 
-	}
+	
+}
 
 }
