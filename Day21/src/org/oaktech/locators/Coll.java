560a06c29a3242508f929663d1ce6bcd4f9f5ad5
ravilella <lellaravi25ster@gmail.com>
2017-09-07 16:06:00 +0530

    Revert "Deleted some lines in class Coll"
    This reverts commit 3a8655a30e543e16354c0003d9a157a6ab24b9ad.

--- Day21/src/org/oaktech/locators/Coll.java
+++ Day21/src/org/oaktech/locators/Coll.java
@@ -14,10 +14,19 @@ public class Coll
 		c=new ArrayList();
 		c.add(10);
 		c.add(20);
-		
+		c.add(20);
+		c.add(30);
+		c.add("ravi");
+		c.add(10.00f);
+		c.add('a');
 		
 		System.out.println(c);
 		
-		
+		Object o1 = c.get(6);
+		if(o1 instanceof Character)
+		{
+			//Integer str = (Integer)o1;
+			System.out.println(o1);
+		}
 	}
 }
