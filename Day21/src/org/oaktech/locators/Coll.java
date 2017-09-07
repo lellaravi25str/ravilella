881d703895c3165614e0417954391eaa7e50f0df
ravilella <lellaravi25ster@gmail.com>
2017-09-07 16:08:22 +0530

    deleted few lines of code in coll class

--- Day21/src/org/oaktech/locators/Coll.java
+++ Day21/src/org/oaktech/locators/Coll.java
@@ -13,20 +13,10 @@ public class Coll
 	{
 		c=new ArrayList();
 		c.add(10);
-		c.add(20);
-		c.add(20);
-		c.add(30);
-		c.add("ravi");
-		c.add(10.00f);
-		c.add('a');
+		
 		
 		System.out.println(c);
 		
-		Object o1 = c.get(6);
-		if(o1 instanceof Character)
-		{
-			//Integer str = (Integer)o1;
-			System.out.println(o1);
-		}
+		
 	}
 }
